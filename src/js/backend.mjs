c192c050dc413ce88f9abb6174af1668b7f42d02
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-02-11 08:10:17 +0100

    commit TD5

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -7,7 +7,7 @@ const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function getEvents() {
     const today = new Date().toISOString(); // Récupérer la date du jour. Regardez la doc de la méthode toISOString pour plus d'informations: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
-    let events = await pb.collection("events").getFullList(
+    let events = await pb.collection("evenement").getFullList(
         {
             sort: "-date",
             order: "desc",
@@ -15,7 +15,7 @@ export async function getEvents() {
         }
     ); // Filtrer les événements passés en fonction de la date. Si la colonne date n'est pas dans votre collection, vous pouvez la remplacer par une autre colonne de type date.
     events = events.map((event) => {
-        event.img = pb.files.getURL(event, event.imgUrl);
+        event.img = pb.files.getURL(event, event.img);
         return event;
     }); // Récupérer l'URL de l'image de chaque événement. Vous pouvez remplacer imgUrl par le nom de la colonne qui contient le nom de l'image.
     events = events.map((event) => {
@@ -27,7 +27,7 @@ export async function getEvents() {
 
 export async function getOneEvent(id) {
     try {
-        const event = await pb.collection("events").getOne(id);
+        const event = await pb.collection("evenement").getOne(id);
         event.img = pb.files.getURL(event, event.imgUrl);
         event.formattedDate = formatDate(event.date);
         return event;
@@ -40,7 +40,7 @@ export async function getOneEvent(id) {
 export async function setFavoriteEvent(id, valeurFavori) {
     try {
         console.log("id", id, "valeurFavori", valeurFavori);
-        await pb.collection("events").update(id, { favori: valeurFavori });
+        await pb.collection("evenement").update(id, { favori: valeurFavori });
         return true;
     } catch (error) {
         console.log("error", error);
